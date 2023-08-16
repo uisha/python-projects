96be321ca659628f2fb4bc42e1178ca98a991426
uisha <joshua_emmanuel_g_tipon@dlsu.edu.ph>
2023-08-17 00:44:01 +0800

    Squirrel Project

--- day 25/main.py
+++ day 25/main.py
@@ -14,5 +14,71 @@
 
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+# data = pandas.read_csv("weather_data.csv")
+# # print(data["temp"])
+#
+#
+# data_dict = data.to_dict()
+# print(data_dict)
+#
+# temp_list = data["temp"].to_list()
+# print(temp_list)
+#
+# ave = sum(temp_list)/len(temp_list)
+# print(data["temp"].mean())
+# print(data["temp"].max())
+#
+# #Get Data in Columns
+# print(data["condition"])
+# print(data.condition)
+
+#Get Data in Rows
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data.temp.max()])
+
+# monday = data[data.day == "Monday"]
+# print((int(monday.temp)*9/5)+32)
+#
+# #Create a dataframe from scratch
+# data_dict = {
+#     "students": ["Amy", "James", "Angela"],
+#     "scores": [76, 56, 65]
+# }
+#
+# data = pandas.DataFrame(data_dict)
+# data.to_csv("new_data.csv")
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+
+# color_list = data["Primary Fur Color"]
+# total_gray = 0
+# total_red = 0
+# total_black = 0
+# for color in color_list:
+#     if color == "Gray":
+#         total_gray += 1
+#     elif color == "Cinnamon":
+#         total_red += 1
+#     elif color == "Black":
+#         total_black += 1
+#
+# color_dict = {
+#     "Fur Color": ["grey", "red", "black"],
+#     "Count": [total_gray, total_red, total_black]
+# }
+#
+# new_data = pandas.DataFrame(color_dict)
+# new_data.to_csv("squirrel_count.csv")
+
+# Correct solution
+gray_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+cinnamon_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+
+data_dict = {
+    "Fur Color": ["grey", "red", "black"],
+    "Count": [gray_squirrels_count, cinnamon_squirrels_count, black_squirrels_count]
+}
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
