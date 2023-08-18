24f29bd78dd4699cb62a9b7621ee9b730b0327bf
uisha <joshua_emmanuel_g_tipon@dlsu.edu.ph>
2023-08-18 17:03:28 +0800

    Functionality done

--- day-25-us-states-game-start/main.py
+++ day-25-us-states-game-start/main.py
@@ -1,10 +1,28 @@
 import turtle
+import pandas
+
+data = pandas.read_csv("50_states.csv")
+states = data.state.to_list()
 
 screen = turtle.Screen()
 screen.title("U.S. States Games")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
+guessed_states = []
+title = "Guess the State"
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's aother state's name?")
-print(answer_state)
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=title, prompt="What's another state's name?").title()
+
+    if answer_state in states:
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+        guessed_states.append(answer_state)
+        title = f"Guessed {len(guessed_states)}/50 States"
+screen.exitonclick()
