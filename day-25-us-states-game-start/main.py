acb81b1fd0a4c8edfeba035fcc0d0110716f3164
uisha <joshua_emmanuel_g_tipon@dlsu.edu.ph>
2023-08-18 17:20:07 +0800

    Day 25 done

--- day-25-us-states-game-start/main.py
+++ day-25-us-states-game-start/main.py
@@ -1,5 +1,6 @@
 import turtle
 import pandas
+import csv
 
 data = pandas.read_csv("50_states.csv")
 states = data.state.to_list()
@@ -15,6 +16,8 @@ title = "Guess the State"
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=title, prompt="What's another state's name?").title()
 
+    if answer_state == "Exit":
+        break
     if answer_state in states:
         t = turtle.Turtle()
         t.hideturtle()
@@ -25,4 +28,15 @@ while len(guessed_states) < 50:
         t.write(answer_state)
         guessed_states.append(answer_state)
         title = f"Guessed {len(guessed_states)}/50 States"
+
+if len(guessed_states) < 50:
+    states_array = []
+    for state in states:
+        if state not in guessed_states:
+            states_array.append(state)
+    states_dict = {
+        "state": states_array
+    }
+    states_to_learn = pandas.DataFrame(states_dict)
+    states_to_learn.to_csv("states_to_learn.csv")
 screen.exitonclick()
