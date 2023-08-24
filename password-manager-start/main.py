f3d77e77c3abe07681c701582f9c2f8ef586e2db
uisha <joshua_emmanuel_g_tipon@dlsu.edu.ph>
2023-08-24 14:43:58 +0800

    sort of done?

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -4,14 +4,16 @@ from random import choice, randint, shuffle
 import pyperclip
 import json
 
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
-#Password Generator Project
+# Password Generator Project
 def generate_password():
     password_input.delete(0, END)
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-
 
     # for char in range(nr_letters):
     #   password_list.append(random.choice(letters))
@@ -30,9 +32,11 @@ def generate_password():
     password = "".join(password_list)
     password_input.insert(0, password)
     pyperclip.copy(password)
+
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
-    website = web_input.get()
+    website = web_input.get().capitalize()
     email = email_input.get()
     password = password_input.get()
     new_data = {
@@ -50,7 +54,7 @@ def save():
                 # Read Old Data
                 data = json.load(data_file)
         except FileNotFoundError:
-            with open("data_file.json", "w") as data_file:
+            with open("data.json", "w") as data_file:
                 json.dump(new_data, data_file, indent=4)
 
         else:
@@ -63,6 +67,31 @@ def save():
         finally:
             web_input.delete(0, END)
             password_input.delete(0, END)
+
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def search_data(data):
+    try:
+        website = web_input.get().capitalize()
+        account = data.get(website)
+        if account is None:
+            raise KeyError("No such key exists")
+    except KeyError:
+        messagebox.showerror(message="No details for the website exists")
+    else:
+        email = account.get("email")
+        password = account.get("password")
+        messagebox.showinfo(message=f"Website: {website}\n"
+                                    f"Email/Username: {email}\n"
+                                    f"Password: {password}")
+def find_password():
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showerror(message="No Data File Found")
+    else:
+        search_data(data)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -86,11 +115,11 @@ email_label.grid(column=0, row=2)
 password_label.grid(column=0, row=3)
 
 # Entries
-web_input = Entry(width=35)
-email_input = Entry(width=35)
+web_input = Entry(width=21)
+email_input = Entry(width=36)
 password_input = Entry(width=21)
 
-web_input.grid(column=1, row=1, columnspan=2)
+web_input.grid(column=1, row=1)
 email_input.grid(column=1, row=2, columnspan=2)
 password_input.grid(column=1, row=3)
 
@@ -99,9 +128,11 @@ email_input.insert(0, "joshtipon921@gmail.com")
 
 # Buttons
 generate_password_button = Button(text="Generate Password", command=generate_password)
-add_button = Button(text="Add", width=36, command=save)
+add_button = Button(text="Add", width=37, command=save)
+search_button = Button(text="Search", width=14, command=find_password)
 
 generate_password_button.grid(column=2, row=3)
 add_button.grid(column=1, row=4, columnspan=2)
+search_button.grid(column=2, row=1)
 
 window.mainloop()
